7179cca911923b77e56fa1ee1c90171d8fc9cdbb
hika019 <ikazuti2001@gmail.com>
2020-12-10 19:18:38 +0900

    今日までの課題を追加

--- app/src/main/java/com/example/kerkar/MainActivity.kt
+++ app/src/main/java/com/example/kerkar/MainActivity.kt
@@ -2,15 +2,41 @@ package com.example.kerkar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.recyclerview.widget.RecyclerView
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
+import com.xwray.groupie.Item
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        
+        val main_assignment_adapter = GroupAdapter<GroupieViewHolder>()
+        main_assignment_adapter.add(AssignmentItem())
+        main_assignment_adapter.add(AssignmentItem())
 
 
-
-
+        main_assignment_info_recyclerview.adapter = main_assignment_adapter
     }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.nav_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+}
+
+class AssignmentItem: Item<GroupieViewHolder>(){
+    override fun bind(viewHolder: GroupieViewHolder, position: Int) {
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.assignment_info
+    }
+
 }
