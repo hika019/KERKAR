40ea4b4632ecb026de84432d26407b49f16182dd
hika019 <ikazuti2001@gmail.com>
2020-12-14 15:27:40 +0900

    main画面include消去

--- app/src/main/java/com/example/kerkar/MainActivity.kt
+++ app/src/main/java/com/example/kerkar/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.kerkar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
 import com.xwray.groupie.Item
 import kotlinx.android.synthetic.main.activity_main.*
+
 import kotlinx.android.synthetic.main.main_assignment_info_item.view.*
 
 
@@ -13,16 +17,46 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         setSupportActionBar(findViewById(R.id.toolbar))
+
+
+        val main_assignment_adapter = GroupAdapter<GroupieViewHolder>()
+        main_assignment_adapter.add(AssignmentItem())
+        main_assignment_adapter.add(AssignmentItem())
+        main_assignment_adapter.add(AssignmentItem())
+        main_assignment_adapter.add(AssignmentItem())
+        main_assignment_adapter.add(AssignmentItem())
+        main_assignment_adapter.add(AssignmentItem())
+        main_assignment_adapter.add(AssignmentItem())
+        main_assignment_adapter.add(AssignmentItem())
+        main_assignment_adapter.add(AssignmentItem())
+        main_assignment_adapter.add(AssignmentItem())
+        main_assignment_adapter.add(AssignmentItem())
+        main_assignment_adapter.add(AssignmentItem())
+        main_assignment_info_recyclerview.adapter = main_assignment_adapter
 
 
 
     }
+}
 
+class AssignmentItem: Item<GroupieViewHolder>(){
+    override fun bind(viewHolder: GroupieViewHolder, position: Int) {
+        //各レポートの内容をいじる
+        //下のはサンプル
+        val tmp = viewHolder.itemView.main_assignmennt_info_subject_textview.text
 
+    }
+    override fun getLayout(): Int {
+        return R.layout.main_assignment_info_item
+    }
+}
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.nav_menu, menu)
-        return super.onCreateOptionsMenu(menu)
+class today_timetable_Item: Item<GroupieViewHolder>(){
+    override fun bind(viewHolder: GroupieViewHolder, position: Int) {
+    }
+    override fun getLayout(): Int {
+        return R.layout.today_taimetable
     }
 }
