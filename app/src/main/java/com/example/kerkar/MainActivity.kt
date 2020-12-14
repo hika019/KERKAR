6a9b0213a811b716f1e29063b840bea1c4ae28c2
hika019 <ikazuti2001@gmail.com>
2020-12-14 10:55:14 +0900

    toolbarへの置換

--- app/src/main/java/com/example/kerkar/MainActivity.kt
+++ app/src/main/java/com/example/kerkar/MainActivity.kt
@@ -3,8 +3,6 @@ package com.example.kerkar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
-import com.xwray.groupie.GroupAdapter
-import com.xwray.groupie.GroupieViewHolder
 import com.xwray.groupie.Item
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.main_assignment_info_item.view.*
@@ -15,46 +13,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setSupportActionBar(findViewById(R.id.toolbar))
 
-        val main_assignment_adapter = GroupAdapter<GroupieViewHolder>()
-        main_assignment_adapter.add(AssignmentItem())
-        main_assignment_adapter.add(AssignmentItem())
-        main_assignment_adapter.add(AssignmentItem())
-        main_assignment_adapter.add(AssignmentItem())
-        main_assignment_adapter.add(AssignmentItem())
-        main_assignment_adapter.add(AssignmentItem())
-        main_assignment_adapter.add(AssignmentItem())
-        main_assignment_adapter.add(AssignmentItem())
-        main_assignment_adapter.add(AssignmentItem())
-        main_assignment_adapter.add(AssignmentItem())
-        main_assignment_adapter.add(AssignmentItem())
-        main_assignment_adapter.add(AssignmentItem())
-        main_assignment_info_recyclerview.adapter = main_assignment_adapter
+
+
     }
+
 
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.nav_menu, menu)
         return super.onCreateOptionsMenu(menu)
-    }
-}
-
-class AssignmentItem: Item<GroupieViewHolder>(){
-    override fun bind(viewHolder: GroupieViewHolder, position: Int) {
-        //各レポートの内容をいじる
-        //下のはサンプル
-        val tmp = viewHolder.itemView.main_assignmennt_info_subject_textview.text
-
-    }
-    override fun getLayout(): Int {
-        return R.layout.main_assignment_info_item
-    }
-}
-
-class today_timetable_Item: Item<GroupieViewHolder>(){
-    override fun bind(viewHolder: GroupieViewHolder, position: Int) {
-    }
-    override fun getLayout(): Int {
-        return R.layout.today_taimetable
     }
 }
