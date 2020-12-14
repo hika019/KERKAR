71ca061804a7a8216e311c3635d0d3d74bb4ee81
hika019 <ikazuti2001@gmail.com>
2020-12-15 01:50:57 +0900

    これだと画面遷移できない

--- app/src/main/java/com/example/kerkar/MainActivity.kt
+++ app/src/main/java/com/example/kerkar/MainActivity.kt
@@ -1,26 +1,43 @@
 package com.example.kerkar
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import android.widget.Toast
 
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
 import com.xwray.groupie.Item
 import kotlinx.android.synthetic.main.activity_main.*
-
 import kotlinx.android.synthetic.main.main_assignment_info_item.view.*
 
 
 class MainActivity : AppCompatActivity() {
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.draw_manu_home -> {
+                Toast.makeText(this, "aiuep", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        
+
+
+
         val main_assignment_adapter = GroupAdapter<GroupieViewHolder>()
         main_assignment_adapter.add(AssignmentItem())
         main_assignment_adapter.add(AssignmentItem())
