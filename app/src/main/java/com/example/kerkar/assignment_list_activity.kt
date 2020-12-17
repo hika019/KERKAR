fb240542657ea41c2f214ec698dbed5135824b5b
hika019 <ikazuti2001@gmail.com>
2020-12-18 01:52:49 +0900

    課題画面修正

--- app/src/main/java/com/example/kerkar/assignment_list_activity.kt
+++ app/src/main/java/com/example/kerkar/assignment_list_activity.kt
@@ -1,22 +1,20 @@
 package com.example.kerkar
 
 import android.content.Intent
+import android.graphics.Typeface
+import android.graphics.Typeface.BOLD
+import android.graphics.Typeface.DEFAULT
 import android.os.Bundle
-import android.view.ContextMenu
-import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_assignment_list.*
-import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.activity_home.nav_view
 
 class assignment_list_activity :AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_assignment_list)
-
         setSupportActionBar(findViewById(R.id.toolbar))
-
         nav_view.setNavigationItemSelectedListener {
             when (it.itemId){
                 R.id.draw_manu_home -> {
@@ -38,11 +36,18 @@ class assignment_list_activity :AppCompatActivity() {
             }
 
         }
-        
-        switch1.setOnCheckedChangeListener { buttonView, isChecked ->
-            if(switch1.isChecked){
+
+
+
+        //提出済み,未提出の切り替え
+        unsubmitted_or_submitted_switch.setOnCheckedChangeListener { buttonView, isChecked ->
+            if(isChecked){
+                submitted_textview.setTypeface(Typeface.DEFAULT_BOLD)
+                unsubmitted_textview.setTypeface(Typeface.DEFAULT)
                 Toast.makeText(this, "true", Toast.LENGTH_SHORT).show()
             }else{
+                unsubmitted_textview.setTypeface(Typeface.DEFAULT_BOLD)
+                submitted_textview.setTypeface(Typeface.DEFAULT)
                 Toast.makeText(this, "false", Toast.LENGTH_SHORT).show()
             }
         }
