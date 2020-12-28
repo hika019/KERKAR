f9e16e4fdbcad7bf125c092c2c0fa31834635f02
hika019 <ikazuti2001@gmail.com>
2020-12-29 03:10:57 +0900

    update item_layout

--- app/src/main/java/com/example/kerkar/db_local.kt
+++ app/src/main/java/com/example/kerkar/db_local.kt
@@ -1,2 +1,16 @@
 package com.example.kerkar
 
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class local_db(context: Context, databaseName: String, factory: SQLiteDatabase.CursorFactory?, version: Int): SQLiteOpenHelper(context, databaseName, factory, version){
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL("create table if not exists SampleTable (id text primary key, name text, type integer, image BLOB)")
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        db?.execSQL("alter table SampleTable add column deleteFlag integer default 0")
+    }
+
+}
